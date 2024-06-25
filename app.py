c4340820a8268e1b5e3e065a8d09630695042e23
serendipi27 <serendipi27@gmail.com>
2024-06-25 22:30:03 +0900

    webapp code uploading

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
-print('hello')
-print('no!!')
-print('go to mart")
+
+import streamlit as st
+
+year = st.slider('연도를 선택하세요', 2000, 2020)
+hobby = st.selectbox('다음 중 취미를 선택하세요', ('읽기', '여행', '요리'))
+st.write(f'당신은 연도는 {year}와 취미는 {hobby}를 선택하셨습니다.')
